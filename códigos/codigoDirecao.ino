45bb2b2503488d40c365e169bf712d575316bc14
Keila Dario <105608713+eoqueila@users.noreply.github.com>
2024-10-14 02:15:27 -0300

    Create codigoDirecao.ino
    Código para controlar a direção dos motores

---
+++ códigos/codigoDirecao.ino
@@ -0,0 +1,33 @@
+//Definicoes pinos Arduino ligados a entrada da Ponte H 
+int IN1 = 5; 
+int IN2 = 6; 
+
+void setup() 
+{ 
+//Define os pinos como saida 
+pinMode(IN1, OUTPUT); 
+pinMode(IN2, OUTPUT); 
+} 
+
+void loop() 
+{ 
+//Gira o Motor A no sentido horario 
+digitalWrite(IN1, HIGH); 
+digitalWrite(IN2, LOW); 
+delay(2000); 
+
+//Para o motor A 
+digitalWrite(IN1, HIGH); 
+digitalWrite(IN2, HIGH); 
+delay(500); 
+
+//Gira o Motor A no sentido anti-horario 
+digitalWrite(IN1, LOW); 
+digitalWrite(IN2, HIGH); 
+delay(2000); 
+
+//Para o motor A 
+digitalWrite(IN1, HIGH); 
+digitalWrite(IN2, HIGH); 
+delay(500); 
+} 
